36eefd58537547ceb493697617b8c074b41dc8d8
AbdelRahman Aref <abdelrahman_aref@hotmail.com>
2018-05-14 09:08:04 +0200

    Initial Commit

---
+++ Notes/View Controllers/Root View Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by AbdelRahman Aref on 5/14/18.
+//  Copyright © 2018 AbdelRahman Aref. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
